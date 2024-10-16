09fd1e00513ab897faf706f860b1f08e02a22835
Ayusht777 <talesarayush@gmail.com>
2024-10-16 09:20:34 +0530

    fix the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
 import { FaPhone } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 const Navbar = () => {
   const handleCall = () => {
-    window.location.href = "tel:+91-931-995-9557";
+    //to open in same tab
+    // window.location.href = "tel:+91-963-010-7798";
+    window.open("tel:+91-963-010-7798", "_blank");
   };
+  const routes = [
+    { label: "Home", path: "/" },
+    { label: "Blogs", path: "/blogs" },
+  ];
   return (
     <nav className="w-full ">
       <div className="w-full flex items-center justify-between lg:px-4 px-2 shadow-sm bg-[#F7F7F7] py-1">
@@ -15,20 +21,30 @@ const Navbar = () => {
         </div>
         <div className="flex h-full items-center justify-center flex-grow">
           <ul className="flex justify-center gap-x-4">
-            <li className="text-[#13253F] font-medium text-center w-full">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="text-[#13253F] font-medium text-center w-full">
-              <Link to="/blogs">Blogs</Link>
-            </li>
+            {routes.map((route, index) => (
+              <li key={index}>
+                <NavLink
+                  to={route.path}
+                  className={({ isActive }) =>
+                    ` ${
+                      isActive
+                        ? "text-[#F37002] before:content-['•'] before:mr-1 before:text-[#F37002]"
+                        : "text-[#202020]"
+                    }`
+                  }
+                >
+                  {route.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="flex h-full items-center justify-end flex-grow">
           <div className="flex items-center gap-x-1 bg-[#F37002] px-4 rounded-2xl py-1">
-            <FaPhone className="text-white" />
+            <FaPhone className="text-[#F7F7F7]" />
             <button
               onClick={handleCall}
-              className="text-white font-semibold uppercase"
+              className="text-[#F7F7F7] font-semibold uppercase"
             >
               Talk to An expert
             </button>

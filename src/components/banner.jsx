ccca506a6997e0061cc3c7ed4ee586198d59f717
ayush-appcrave <ayush.talesara777.appcrave@gmail.com>
2024-10-25 14:35:49 +0530

    Update banner.jsx

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,11 +1,11 @@
 import { FaArrowRight, FaGift } from "react-icons/fa";
 
 const SalesBanner = () => {
-  const gradientClasses = "bg-gradient-to-br from-[#13253F] to-[#0F1E32]";
+  const gradientClasses = "bg-gradient-to-br from-[#f57725] to-[#fcaf17]";
   const buttonClasses =
     "text-[#0F1E32] bg-white hover:bg-gray-50 px-4 py-2 rounded-lg w-fit ease duration-300 flex gap-1 items-center group";
 
-  const phoneNumber = "+91-9319959557";
+  const phoneNumber = "+91-9630107798";
 
   return (
     <section className="px-4">
